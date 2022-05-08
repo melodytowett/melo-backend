118b7db07f14f57dbecfd53729a4b2d7aa9528d2
melodytowett <melodytowett99@gmail.com>
2022-05-08 10:13:51 +0300

    added gettext impotrt

--- route/serializer.py
+++ route/serializer.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from django.utils.translation import gettext_lazy as _
 from route .models import Manager,Merchandiser,User,Address
-from rest_auth.registration.serializers import RegisterSerializer
 from django.contrib.auth import authenticate
 
 class UserSerializer(serializers.ModelSerializer):
