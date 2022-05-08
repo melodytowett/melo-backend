8db4c10376a9ae9efefc7078fa920c1665c81664
melodytowett <melodytowett99@gmail.com>
2022-05-08 10:20:18 +0300

    added gettext impotrt

--- merchandisers/urls.py
+++ merchandisers/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('route.urls')),
     path('api-auth/',include('rest_framework.urls')),
-    path('rest-auth/',include('rest_auth.urls')),
+    # path('rest-auth/',include('rest_auth.urls')),
     path('rest-auth/registration/',include('rest_auth.registration.urls')),
     path('api/',include('route.urls',namespace='api')),
     path('api-toke-auth/',obtain_auth_token)
